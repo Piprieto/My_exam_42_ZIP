7b6c617d13ecbc1b1ed59033158969f1261ef7eb
Pilar Prieto <163329583+Piprieto@users.noreply.github.com>
2025-12-02 10:18:18 +0100

    Improve comments and formatting in inter.c
    Refactor comments and formatting for clarity in inter.c

--- level_06/inter.c
+++ level_06/inter.c
@@ -1,4 +1,9 @@
-/*Escribe un programa que tome dos cadenas y muestre, sin dobles, los
+/*			06_inter.txt										
+			
+Assignment name  : inter
+Expected files   : inter.c
+Allowed functions: write
+Escribe un programa que tome dos cadenas y muestre, sin dobles, los
 caracteres que aparecen en ambas cadenas, en el orden en que aparecen en la primera.
 La pantalla será seguida por un \n.
 Si el número de argumentos no es 2, el programa muestra \n.
@@ -16,17 +21,17 @@ $*/
 
 #include <unistd.h>
 
-int ft_iter(char *str, int i)							//función para comprobar que un carácter está repetido en una cadena
+int ft_iter(char *str, int i)			//función para comprobar que un carácter está repetido en una cadena
 {			
 	int j = 0;
 	
-	while(j < i) 								//bucle para recorrer la cadena desde la posición 0 hasta la i
+	while(j < i) 						//bucle para recorrer la cadena desde la posición 0 hasta la i
 	{
-		if(str[i] == str[j]) 						//si el carácter se repite devuelve 1
+		if(str[i] == str[j]) 			//si el carácter se repite devuelve 1
 			return(1);
 		j++;
 	}
-	return(0); 								//si el carácter no aparecía antes, devuelve 0
+	return(0); 							//si el carácter no aparecía antes, devuelve 0
 }
 
 int	main(int argc, char **argv)
@@ -34,14 +39,14 @@ int	main(int argc, char **argv)
 	int j;
 	int i = 0;
 	
-	if (argc == 3) 								//verificamos que se han pasado dos cadenas para comparar
+	if (argc == 3) 													//verificamos que se han pasado dos cadenas para comparar
 	{
-		while(argv[1][i] != '\0') 					// analizaremos cada carácter de la primera cadena con cada uno de la segunda 
-		{			  					//y a su vez con los anteriores de la primera, para que no se repitan.
-			j = 0; 							//j recorre la segunda cadena tantas veces como caracteres tenga la primera
+		while(argv[1][i] != '\0') 									// analizaremos cada carácter de la primera cadena con cada uno de la segunda 
+		{			  												//y a su vez con los anteriores de la primera, para que no se repitan.
+			j = 0; 													//j recorre la segunda cadena tantas veces como caracteres tenga la primera
 			while(argv[2][j] != '\0' && ft_iter(argv[1], i) == 0) 	//si no hemos llegado al final de la segunda cadena
-			{							//y si el carácter a comparar de la primera no se ha comparado ya anteriormente
-				if (argv[1][i] == argv[2][j])			// si aparece en las dos cadenas lo escribo una vez y salgo
+			{														//y si el carácter a comparar de la primera no se ha comparado ya anteriormente
+				if (argv[1][i] == argv[2][j])						// si aparece en las dos cadenas lo escribo una vez y salgo
 				{
 					write(1, &argv[1][i], 1);
 					break; 
