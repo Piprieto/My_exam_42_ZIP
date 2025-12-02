bc98c0a351199ff76e1b1d159b4abdafa62b86c2
Pilar Prieto <163329583+Piprieto@users.noreply.github.com>
2025-12-02 10:25:18 +0100

    Refactor ft_itoa to support long int conversion
    Refactor ft_itoa function to handle long int and improve readability.

--- level_07/ft_itoa.c
+++ level_07/ft_itoa.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 /*			07_ft_itoa.txt		
 			
 Assignment name  : ft_itoa
@@ -16,18 +13,18 @@ Escribe una función que tome un int y lo convierta en una cadena terminada en n
 La función devuelve el resultado en una matriz de caracteres que debes asignar.
 Su función debe declararse de la siguiente manera:
 char *ft_itoa(int nbr);
-*/
 
-/* 
-Queremos convertir un int en una cadena de char. Un int son 64 bytes (32 bits) así que permite almacenar valores entre -2147483648 y 2147483647. Si le damos a la func como argumento un número mayor dará error y los dos números límites tampoco saldrán correctamente pq hay que tratarlos diferente. Así que aunque el arg de la función que nos piden es int, lo cambiaremos a long int permitiendo almacenar valores mucho mayores.
+Queremos convertir un int en una cadena de char. Un int son 64 bytes (32 bits) así que permite almacenar 
+valores entre -2147483648 y 2147483647. Si le damos a la func como argumento un número mayor dará error y 
+los dos números límites tampoco saldrán correctamente pq hay que tratarlos diferente. Así que aunque el 
+arg de la función que nos piden es int, lo cambiaremos a long int permitiendo almacenar valores mucho mayores.
 Para obtener el número max INT en la terminal:
 un "int" esta formado por 32 bits
 Positivos y negativos (enteros con signo): del -2147483648 al 2147483647
-
-En un terminal ejecuta "bc"  (Basic Calculator)
-pones 2 ^ 31 y obtienes 2147483648
-para salir "quit"
 */
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int ft_nbrlen(long int nbr)      // Función para contar los digitos del número y el signo en caso de negativo
 {
@@ -35,17 +32,17 @@ int ft_nbrlen(long int nbr)      // Función para contar los digitos del número
 	
 	if (nbr == 0)
 		i = 1;
-	if (nbr < 0)			// Si el número es menor de 0 (negativo)
+	if (nbr < 0)				// Si el número es menor de 0 (negativo)
 	{
-		i++;			// Cuento uno por el signo negativo
-		nbr = nbr * (-1);	// convierto el número en positivo
+		i++;					// Cuento uno por el signo negativo
+		nbr = nbr * (-1);		// convierto el número en positivo
 	}
-	while (nbr > 0)			// Recorro el número mientras sea mayor que 0
+	while (nbr > 0)				// Recorro el número mientras sea mayor que 0
 	{
-		i++;			// Cuento el digito
-		nbr = nbr / 10;		// Divido el número entre 10 (me muevo entre unidades, decenas, centenas...)
+		i++;					// Cuento el digito
+		nbr = nbr / 10;			// Divido el número entre 10 (me muevo entre unidades, decenas, centenas...)
 	}
-	return(i);			// Devuelvo el número de digitos incluyendo el signo en caso de negativo
+	return(i);					// Devuelvo el número de digitos incluyendo el signo en caso de negativo
 }
 
 char	*ft_itoa(int nbr)			
@@ -58,21 +55,21 @@ char	*ft_itoa(int nbr)
 	n = (long int)nbr;				//pasamos nbr de int a long int 
 	len = ft_nbrlen(n);				//calculamos el número de dígitos de nbr
 	str = (char*)malloc(sizeof(char) * (len + 1));	//asignamos espacio para cada carácter y también para el cierre de string '/0'
-	if(!str)					//protejo el malloc
+	if(!str)						//protejo el malloc
 		return(NULL);	
 	str[len] = '\0';				//colocamos el cierre del string
 	if(n == 0)
 		str[--len] = '0';
-	if (n < 0)					//si el número es negativo 
+	if (n < 0)						//si el número es negativo 
 	{
 		str[0] = '-';				//colocamos el signo
-		n = n * (-1);			//hacemos que sea positivo
+		n = n * (-1);				//hacemos que sea positivo
 	}
 	while (n > 0)
 	{
-		len --;				//nos colocamos en la posición anterior
+		len --;						//nos colocamos en la posición anterior
 		str[len] = digits[n % 10];	//asigno el digito obtenido del resto de la división entre 10.
-        	n = n / 10;			// Divido el número entre 10 (me muevo entre unidades, decenas, centenas...)
+        	n = n / 10;				// Divido el número entre 10 (me muevo entre unidades, decenas, centenas...)
 	}
 	return(str);
 	
